5789d78e3079e11961c62d6fb3715c4c75bd9b37
Nikhil14 <nmurark1@binghamton.edu>
2014-02-18 22:26:10 -0500

    Inital Map Class

--- main.cpp
+++ main.cpp
@@ -1,8 +1,51 @@
 #include<iostream>
+#include<utility>
+#include<string>
+class Map{
+    public:
+        Map();
+        ~Map();
+    private:
+        class Node{
+            public:
+                Node* left;
+                Node* right;
+                Node* parent;
+                short type;//right or left child
+                std::pair<int,int> obj;
+                //constructor
+                Node(std::pair<int,int> &obj);
+                //destructor
+                ~Node();
+        };
+        Node *root;
+        Node *begin;
+        Node *end;
+};
+
+Map::Map():root(nullptr),begin(nullptr),end(nullptr){}
+
+Map::~Map()
+{
+    delete root;
+    delete begin;
+    delete end;
+}
+
+Map::Node::Node(std::pair<int,int> &obj):left(nullptr),right(nullptr),parent(nullptr),type(0),obj(obj){}
+
+Map::Node::~Node()
+{
+    delete Map::Node::left;
+    delete Map::Node::right;
+    delete Map::Node::parent;
+}
 
 int main()
 {
-    std::cout<<"Hello Git"<<'\n';
+
+
+
+
     return 0;
 }
-
