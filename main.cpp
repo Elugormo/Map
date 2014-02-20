d9c9eabf831d861d7ea58c5ee68c9c0f42b58f9a
Nikhil14 <nmurark1@binghamton.edu>
2014-02-19 22:10:19 -0500

    initializer_list added

--- main.cpp
+++ main.cpp
@@ -1,12 +1,16 @@
 #include<iostream>
 #include<utility>
 #include<string>
+#include<cassert>
+#include<initializer_list>
 using Key = std::string;
+using const_Key = const std::string;
 using Value =int;
 class Map{
     public:
         Map();
         ~Map();
+        Map(std::initializer_list<std::pair<Key,Value>>);
         void insert(std::pair<Key,Value> &obj);
         class Node{
             public:
@@ -18,7 +22,9 @@ class Map{
                 Node(std::pair<Key,Value> &obj);
                 ~Node();
         };
-        Node& find_position(Node& ,Key k);
+        Node* find_position(Node* ,Key k);
+        void delete_all_nodes();
+        void delete_all_nodes(Node* n);
         Node *root;
         Node *begin;
         Node *end;
@@ -28,63 +34,85 @@ Map::Map():root(nullptr),begin(nullptr),end(nullptr){}
 
 Map::~Map()
 {
-    delete root;
-    delete begin;
-    delete end;
+    this->delete_all_nodes();
+}
+
+void Map::delete_all_nodes()
+{
+    this->delete_all_nodes(this->root);
+}
+
+void Map::delete_all_nodes(Node *n)
+{   if(n == nullptr)
+        return;
+    delete_all_nodes(n->left);
+    delete_all_nodes(n->right);
+    delete n;
 }
 
 Map::Node::Node(std::pair<Key,Value> &obj):left(nullptr),right(nullptr),parent(nullptr),type(0),pair_obj(obj){}
 
-Map::Node::~Node()
+//I dont need to write the node destructor;
+Map::Node::~Node(){}
+//add const key 
+Map::Map(std::initializer_list<std::pair<Key,Value>> list):root(nullptr),begin(nullptr),end(nullptr)
 {
-    delete Map::Node::left;
-    delete Map::Node::right;
-    delete Map::Node::parent;
+    for(auto i :list)
+    {      
+        this->insert(i);
+    }
 }
 
-Map::Node& Map::find_position(Map::Node& n ,Key k)
+
+Map::Node* Map::find_position(Map::Node* n ,Key k)
 {
-    if(n.pair_obj.first == k)return n;
-    else if(n.pair_obj.first < k)
+    assert(n!=nullptr);
+    if(n->pair_obj.first == k)return n;
+    else if(n->pair_obj.first < k)
     {
-        if(n.right ==  nullptr)
+        if(n->right ==  nullptr)
             return n;
         else
-            return find_position(*n.right,k);
+            return find_position(n->right,k);
     }
-    else if(n.pair_obj.first > k)
+    else if(n->pair_obj.first > k)
     {
-        if(n.left == nullptr)
+        if(n->left == nullptr)
             return n;
         else
-            return find_position(*n.left,k);
+            return find_position(n->left,k);
     }
+
 }
 
 void Map::insert(std::pair<Key,Value> &obj)
 {    
-    Node *n1 = new Node(obj);
+    //Node *n1 = new Node(obj);
     if(this->root == nullptr)
-    {   
-        std::cout<<"hie"<<'\n';
-       // Node *n1 = new Node(obj);
+    {   Node *n1 = new Node(obj); 
         this->root = n1;
         return;
     }
     else
     {   
-        Key k ="Hello";
-        Node& n = find_position(*this->root,obj.first); 
-        if(n.pair_obj.first == obj.first);
-        else if(n.pair_obj.first < obj.first)
-        {   
-            n.type = 0;//right node
-            n.right =n1; 
+        Node* n = find_position(this->root,obj.first); 
+        if(n->pair_obj.first == obj.first)
+        {  
+            return;
         }
-        else if(n.pair_obj.first > obj.first)
+        else if(n->pair_obj.first < obj.first)
+        { 
+            Node *n1 = new Node(obj);
+            n->type = 0;//right node
+            n->right =n1; 
+            n1->parent = n;
+        }
+        else if(n->pair_obj.first > obj.first)
         {   
-            n.type = 1;//left node
-            n.left = n1;
+            Node *n1 = new Node(obj);
+            n->type = 1;//left node
+            n->left = n1;
+            n1->parent = n;
         }
     }
 }
@@ -93,30 +121,6 @@ void Map::insert(std::pair<Key,Value> &obj)
 
 int main()
 {
-    Map m;
-    std::pair<Key,Value> p1("x",14);
-    std::pair<Key,Value> p2("e",13);
-    std::pair<Key,Value> p3("y",14);
-    std::pair<Key,Value> p4("i",13);
-    std::pair<Key,Value> p5("j",14);
-    std::pair<Key,Value> p6("f",13);
-    std::pair<Key,Value> p7("d",14);
-    std::pair<Key,Value> p8("a",13);
- 
- 
-    //std::cout<<"Value::"<<p1.first<<'\n';
-    m.insert(p1);
-    m.insert(p2);
-    m.insert(p3);
-    m.insert(p4);
-    m.insert(p5);
-    m.insert(p6);
-    m.insert(p7);
-    m.insert(p8);
-    
-    std::cout<<m.root->pair_obj.first<<'\n';
-    std::cout<<m.root->left->pair_obj.first<<'\n';
-    
-
+    Map m{{"n",1},{"i",2},{"k",3},{"h",4},{"i",5},{"l",6}};  
     return 0;
 }
